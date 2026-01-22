5a3b081f72384d689ba284d6a71b73d8617524a8
MixsonV <92906194+mixsonv@users.noreply.github.com>
2026-01-22 12:58:46 +0700

    stage 1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/MixsonV/java-project-78/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/MixsonV/java-project-78/actions)
+[![Code Smells](https://sonarcloud.io/api/project_badges/measure?project=MixsonV_java-project-78&metric=code_smells)](https://sonarcloud.io/summary/new_code?id=MixsonV_java-project-78)
---
+++ app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.github.ben-manes.versions") version "0.53.0"
+    id("org.sonarqube") version "7.2.2.6593"
+    application
+    checkstyle
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "MixsonV_java-project-78")
+        property("sonar.organization", "mixsonv")
+    }
+}
