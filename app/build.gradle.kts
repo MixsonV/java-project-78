a15e74d9488d7fb6d5413bfc85381a29408ba779
MixsonV <92906194+mixsonv@users.noreply.github.com>
2026-01-28 13:17:42 +0700

    fix all 2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.github.ben-manes.versions") version "0.53.0"
     id("org.sonarqube") version "7.2.2.6593"
     id("java")
+    jacoco
     checkstyle
 }
 
@@ -19,11 +20,23 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
 }
 
 sonar {
     properties {
         property("sonar.projectKey", "MixsonV_java-project-78")
         property("sonar.organization", "mixsonv")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.coverage.jacoco.xmlReportPaths",
+            "${layout.buildDirectory.get()}/reports/jacoco/test/jacocoTestReport.xml")
     }
 }
